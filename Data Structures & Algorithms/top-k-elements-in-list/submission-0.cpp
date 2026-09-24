310daf9802c84a3acbf69eb1c705dfa2698ed527
Chirag Khati <145248783+potatotify@users.noreply.github.com>
2026-09-24 13:46:49 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        unordered_map<int, int> freq;
+
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        
+        vector<vector<int>> bucket(nums.size() + 1);
+
+        
+        for (auto& [num, count] : freq) {
+            bucket[count].push_back(num);
+        }
+
+        
+        vector<int> ans;
+
+        for (int i = nums.size(); i >= 1; i--) {
+            for (int num : bucket[i]) {
+                ans.push_back(num);
+
+                if (ans.size() == k) {
+                    return ans;
+                }
+            }
+        }
+
+        return ans;
+        
+    }
+};
